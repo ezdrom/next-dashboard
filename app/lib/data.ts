068fe7f135263cbd544e01743ffc9bfb600424d5
im your pc <ezdrom@gmail.com>
2024-03-07 15:20:44 +0800

    add error functionalities

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-    
+
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
@@ -169,7 +169,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
